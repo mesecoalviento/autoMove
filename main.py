3eaccd516880581e356bdaca94e1e8c67868d2b6
mesecoalviento <mesecoalviento@gmail.com>
2017-08-21 20:20:15 +0200

    last update

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python
 from xSlopeClass import xSlopeClass
+import RPi.GPIO as GPIO
+import time
+import math
 
+GPIO.setmode(GPIO.BOARD)
 
-x = xSlopeClass()
-x.xSlope()
+GPIO.setup(7,GPIO.OUT)
+
+try:
+    while True:
+        
+        x = xSlopeClass()
+        y = x.xSlope()
+        z = int(round(math.radians(y)))
+        v = -z
+        
+        GPIO.output(7,z)
+	time.sleep(0.0015)
+	GPIO.output(7,1)
+
+	time.sleep(2)
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
 
